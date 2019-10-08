da36d43c4bc157cfd1318a94151c78f8027a165d
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2019-10-07 21:17:31 -0300

    Wording fixing

--- src/components/Services/ServicesForm.jsx
+++ src/components/Services/ServicesForm.jsx
@@ -50,17 +50,17 @@ class ServicesForm extends Component {
     const { service } = this.props
     return(
       <React.Fragment>
-        <Field className="pl-5 pr-5" label="¿En cual sucursal querés ofrecer este servicio">
+        <Field className="pl-5 pr-5" label="¿En cuál sucursal querés ofrecer este servicio">
           <Select className="is-fullwidth" icon={ faChevronDown } borderless placeholder="Sucursales"/>
         </Field>
-        <Field className="pl-5 pr-5" label="¿Como se llama el servicio que querés ofrecer?">
+        <Field className="pl-5 pr-5" label="¿Cómo se llama el servicio que querés ofrecer?">
           <TextInput value={ service && service.name } name="name" className="is-fullwidth" onChange={ this.handleChange }/>
         </Field>
-        <Field className="pl-5 pr-5" label="¿Cuanto deseas cobrar por el servicio">
+        <Field className="pl-5 pr-5" label="¿Cuánto deseas cobrar por el servicio">
           <TextInput value={ service && service.price } className="is-fullwidth" validate={ (value) => (!priceRegex.test(value)) } name="cost" onChange={ this.handleChange }/>
         </Field>
-        <Field className="pl-5 pr-5" label="¿Cuanto tiempo toma el servicio">
-          <Select name="duration" className="is-fullwidth" value={ service && service.duration } onChange={ this.handleChange } options={ this.getDurationOptions() } icon={ faChevronDown } borderless placeholder="Horarios"/>
+        <Field className="pl-5 pr-5" label="¿Cuánto tiempo toma el servicio?">
+          <Select name="duration" className="is-fullwidth" value={ service && service.duration } onChange={ this.handleChange } options={ this.getDurationOptions() } icon={ faChevronDown } borderless placeholder="Minutos"/>
         </Field>
       </React.Fragment> )
   }
