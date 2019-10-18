208d7ee188bffa2cd4a0fff6f4c37cb97f4a341d
Pedro Gutierrez <pedrog4747@gmail.com>
2019-10-18 20:47:56 -0300

    Added validations for Services creation and edit

--- src/lib/Regex.js
+++ src/lib/Regex.js
@@ -2,3 +2,4 @@ export const nameRegex = /^[a-zA-Z]+$/
 export const dniRegex = /^[0-9]*\.?[0-9]*\.?[0-9]*$/ 
 export const phoneRegex = /^\d+$/
 export const mailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+export const priceRegex = /^\d+$/
