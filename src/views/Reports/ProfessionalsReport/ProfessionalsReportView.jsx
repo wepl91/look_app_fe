46458135ae63733639ad272054c3dd9f77c3d8ec
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2019-11-09 21:52:40 -0300

    Chart config and visual improvement

--- src/views/Reports/ProfessionalsReport/ProfessionalsReportView.jsx
+++ src/views/Reports/ProfessionalsReport/ProfessionalsReportView.jsx
@@ -23,21 +23,6 @@ import {
   DateTimePicker,
 } from 'shipnow-mercurio';
 
-const data = {
-  labels: ['Juan', 'Pedro', 'Bruno', 'Walter', 'Jeremias', 'Nicolas', 'Agustin'],
-  datasets: [
-    {
-      label: 'Ingresos',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65.5, 59, 80, 81, 56, 55, 49],
-    }
-  ]
-};
-
 @observer
 class ProfessionalsReportView extends Component {
   constructor(props) {
@@ -46,6 +31,48 @@ class ProfessionalsReportView extends Component {
 
   getText( text ) {
     return translate(text, this.props.store.ui.language)
+  }
+
+  getChartData() {
+    return {
+      labels: ['Juan', 'Pedro', 'Bruno', 'Walter', 'Jeremias', 'Nicolas', 'Agustin'],
+      datasets: [
+        {
+          label: 'Ingresos',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [65.5, 59, 80, 81, 56, 55, 49],
+        }
+      ]
+    };
+  }
+
+  getChartConfig() {
+    return {
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: this.getText('Ingresos por profesionales'),
+        fontSize: '18',
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem) {
+            return `Total facturado: $${tooltipItem.yLabel}`
+          },
+          title: function(tooltipItem) {
+            debugger
+            return `Ingresos generados por ${tooltipItem[0].xLabel}`
+          } 
+        }
+      }
+    }
   }
 
   render() {
@@ -79,20 +106,10 @@ class ProfessionalsReportView extends Component {
           </Columns>
         <Columns className="pt-5">
         <Bar
-          data={data}
+          data={this.getChartData()}
           width={100}
           height={450}
-          options={{
-            maintainAspectRatio: false,
-            title: {
-              display: true,
-              text: this.getText('Ingresos por profesionales'),
-              fontSize: '18',
-            },
-            legend: {
-              display: false,
-            }
-          }}
+          options={this.getChartConfig()}
       />
         </Columns>
       </React.Fragment> )
